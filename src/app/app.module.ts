980f9962bfada5bf2b552b582c0b7ece553bec33
Cécile <acecileferrier@gmail.com>
2017-08-19 18:19:48 +0100

    Added topbar. Not a menu yet

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TopMenuComponent } from './navigation/top-menu.component';
+import { MdIconModule, MdMenuModule } from '@angular/material';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopMenuComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MdMenuModule,
+    MdIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/navigation/top-menu.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'cf-top-menu',
+  templateUrl: './top-menu.layout.html',
+  styleUrls: ['./top-menu.scss']
+})
+
+export class TopMenuComponent {
+
+}
